41c28eb3c8c2df4b93e4a79c19f55502dd016bc8
Pritam Maharjan <primgdev@gmail.com>
2024-09-29 14:27:32 +1000

    UI setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background': "url('https://res.cloudinary.com/dedpvue13/image/upload/v1727583622/pexels-david-besh-884788_ati9tw.jpg')",
+      }
+    },
   },
   plugins: [],
 }
