bb95f3a343938303690faf5e33cb1219f6e37384
Pritam Maharjan <primgdev@gmail.com>
2024-10-04 19:06:41 +1000

    loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div>
+      <div class="loader w-6 h-6 border-4 border-gray-200 border-t-green-400 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
